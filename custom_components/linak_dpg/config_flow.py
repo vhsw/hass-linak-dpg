d7230f49c235587304d76fa1f06bec7c937cd02a
Alexey Dubrov <nevermind1025@gmail.com>
2022-02-14 13:05:58 +0300

    fix import

--- custom_components/linak_dpg/config_flow.py
+++ custom_components/linak_dpg/config_flow.py
@@ -1,79 +1,73 @@
 """
 Config Flow for Linak DPG Desk Panel Integration
 """
-
+import re
 import time
 
+import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.exceptions import ConfigEntryNotReady
 
-import homeassistant.helpers.config_validation as cv
-
-from .const import LOGGER, DOMAIN
 from .btctl import BTctl
+from .const import DOMAIN, LOGGER
 
 DATA_SCHEMA = vol.Schema(
     {
         vol.Required("name", default="Linak Desk"): cv.string,
-        vol.Required("address"): cv.string
+        vol.Required("address"): cv.string,
     }
 )
 
 
 class LinakDPGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a Linak DPG config flow."""
-    
+
     def __init__(self):
         """Initialize flow."""
         self._address = None
         self._name = None
         self._id = None
-        
+
     def _get_entry(self):
-        data = {
-            "address": self._address,
-            "id": self._id,
-            "name": self._name
-        }
+        data = {"address": self._address, "id": self._id, "name": self._name}
 
         return self.async_create_entry(
             title=self._title,
             data=data,
         )
-        
+
     def _try_connect(self):
         """Try to connect."""
         try:
             wrapper = BTctl()
             wrapper.scan_start()
-            
-            info = wrapper.device_info(self._address);
-            
+
+            info = wrapper.device_info(self._address)
+
             if info:
                 pair = wrapper.pair(self._address)
-            
+
                 if pair:
                     connection = wrapper.connect(self._address)
-                    
+
                     if not connection:
                         raise Exception(f"Failed to establish connection")
-                        
+
                 else:
                     raise Exception(f"Pairing failed")
-                    
+
             else:
                 raise Exception(f"Desk unavailable")
-                
+
         except Exception as e:
             wrapper.scan_stop()
             return e.message
-            
+
         else:
             wrapper.scan_stop()
             return True
-        
+
     async def async_step_import(self, user_input=None):
         """Handle configuration by yaml file."""
         return await self.async_step_user(user_input)
@@ -83,10 +77,13 @@ class LinakDPGConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             if len(user_input["name"]) < 3:
                 raise Exception(f"Name must be atleast 3 characteres.")
-            
-            if re.match('^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$', user_input["address"]) is None:
+
+            if (
+                re.match("^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$", user_input["address"])
+                is None
+            ):
                 raise Exception(f"MAC address is invalid.")
-            
+
             await self.async_set_unique_id(user_input["address"])
             self._abort_if_unique_id_configured()
 
